4df3d92d274ef7b687c5f89a4b2e4ca92035b724
yuyunchen <yuyunchen@ctrip.com>
2019-11-18 14:41:47 +0800

    first commit

---
+++ src/cn/cyy/plugin/trans/TransAction.java
@@ -0,0 +1,53 @@
+package cn.cyy.plugin.trans;
+
+import cn.cyy.plugin.trans.constant.Constant;
+import cn.cyy.plugin.trans.utils.HttpUtil;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.SelectionModel;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.ui.popup.Balloon;
+import com.intellij.openapi.ui.popup.JBPopupFactory;
+import com.intellij.ui.JBColor;
+import org.apache.http.util.TextUtils;
+
+import java.awt.*;
+
+public class TransAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+//        Messages.showMessageDialog("测试插件!", "错误", Messages.getErrorIcon());
+        final Editor mEditor = e.getData(PlatformDataKeys.EDITOR);
+        if (null == mEditor) {
+            return;
+        }
+        SelectionModel model = mEditor.getSelectionModel();
+        final String selectedText = model.getSelectedText();
+        if (TextUtils.isEmpty(selectedText)) {
+            return;
+        }
+        String response = HttpUtil.doGet(Constant.baseUrl + selectedText);
+        String s = String.format("获取到的文字是:%s, \r\n 结果是： %s", selectedText, response);
+
+
+        System.out.println(s);
+        showPopupBalloon(mEditor, s);
+    }
+
+    private void showPopupBalloon(final Editor editor, final String result) {
+        ApplicationManager.getApplication().invokeLater(new Runnable() {
+            public void run() {
+                JBPopupFactory factory = JBPopupFactory.getInstance();
+                factory.createHtmlTextBalloonBuilder(result, null, new JBColor(new Color(186, 238, 186), new Color(73, 117, 73)), null)
+                        .setFadeoutTime(5000)
+                        .createBalloon()
+                        .show(factory.guessBestPopupLocation(editor), Balloon.Position.below);
+            }
+        });
+    }
+}
---
+++ src/cn/cyy/plugin/trans/constant/Constant.java
@@ -0,0 +1,7 @@
+package cn.cyy.plugin.trans.constant;
+
+public interface Constant {
+
+    public static final String baseUrl = "http://fanyi.youdao.com/openapi.do?keyfrom=Skykai521&key=977124034&type=data&doctype=json&version=1.1&q=";
+
+}
